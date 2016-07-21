eab0fed310328116a0e5e99ebf0a50f912723116
Jeroen Frijters <jeroen@frijters.net>
2016-07-21 12:33:35 +0200

    Added test project.

---
+++ test/Managed.Reflection.Tests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+using Managed.Reflection;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            var universe = new Universe();
+            var typeofTests = universe.Import(typeof(Tests));
+            Assert.Equal(typeof(Tests).FullName, typeofTests.FullName);
+            Assert.Equal(typeof(Tests).AssemblyQualifiedName, typeofTests.AssemblyQualifiedName);
+        }
+    }
+}
